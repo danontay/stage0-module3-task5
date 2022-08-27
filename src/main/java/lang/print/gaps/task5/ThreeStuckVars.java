290a6dcc9f269299bb2e3247e8f83c439b4dd7f5
Dana <d.kydyrbayeva>
2022-08-27 17:22:51 +0600

    lucky

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        System.out.println();
+        int a=1, b=10, c=100;
+        System.out.println(a+b+c);
     }
 }
